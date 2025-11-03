16d57feccf3af10c827e944460f95c129984a609
Zair Trivino <trivinoszair@gmail.com>
2025-11-02 22:18:50 -0500

    modificado

--- travelfroned/app/src/main/java/com/example/travelmarket/views/ui/auth/login/LoginScreen.kt
+++ travelfroned/app/src/main/java/com/example/travelmarket/views/ui/auth/login/LoginScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.travelmarket.core.network.NetworkResult
 import com.example.travelmarket.core.storage.TokenManager
@@ -27,12 +26,14 @@ import com.example.travelmarket.views.ui.auth.components.AuthButton
 import com.example.travelmarket.views.ui.auth.components.AuthHeader
 import com.example.travelmarket.views.ui.auth.components.AuthTextField
 import com.example.travelmarket.views.ui.auth.components.AuthTextButton
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 
 @Composable
 fun LoginScreen(
     navController: NavController,
-    viewModel: LoginViewModel = hiltViewModel(),
-    tokenManager: TokenManager = hiltViewModel()
+    viewModel: LoginViewModel = koinViewModel(),
+    tokenManager: TokenManager = koinInject()
 ) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
--- travelfroned/app/src/main/java/com/example/travelmarket/views/ui/auth/register/RegisterScreen.kt
+++ travelfroned/app/src/main/java/com/example/travelmarket/views/ui/auth/register/RegisterScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.travelmarket.core.network.NetworkResult
 import com.example.travelmarket.logic.viewmodels.auth.RegisterViewModel
@@ -24,11 +23,12 @@ import com.example.travelmarket.views.ui.auth.components.AuthButton
 import com.example.travelmarket.views.ui.auth.components.AuthHeader
 import com.example.travelmarket.views.ui.auth.components.AuthTextField
 import com.example.travelmarket.views.ui.auth.components.AuthTextButton
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun RegisterScreen(
     navController: NavController,
-    viewModel: RegisterViewModel = hiltViewModel()
+    viewModel: RegisterViewModel = koinViewModel()
 ) {
     var username by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
